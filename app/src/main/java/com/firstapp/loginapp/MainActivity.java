d6159e3e420a6c2aebabf7705a4748fdaba52f72
harsha <harshavardhanreddyb07@gmail.com>
2022-05-22 16:04:19 +0530

    Initial commit

---
+++ app/src/main/java/com/firstapp/loginapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.firstapp.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    // Intent: move to another activity an clicking a button
+    // add Onclick = "Signup_page" to button in xml file
+    public void SignUp_page(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}
