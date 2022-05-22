13c2c2b3cc0970cc9313f5de800ea1c9f795fffa
harsha <harshavardhanreddyb07@gmail.com>
2022-05-22 21:46:30 +0530

    Signup Template finished

--- app/src/main/java/com/firstapp/loginapp/SignUpActivity.java
+++ app/src/main/java/com/firstapp/loginapp/SignUpActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,5 +21,19 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+
+
+
+    }
+
+
+    public void Google_btn(View view) {
+        Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+        startActivity(intent1);
+    }
+
+    public void Fb_btn(View view) {
+        Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com"));
+        startActivity(intent1);
     }
 }
