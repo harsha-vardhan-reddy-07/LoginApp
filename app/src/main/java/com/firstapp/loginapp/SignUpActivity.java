665924414063527dd84547a2e7257113dcc4d9ac
harsha <harshavardhanreddyb07@gmail.com>
2022-05-22 17:36:51 +0530

    ActionBar is added to signup activity

--- app/src/main/java/com/firstapp/loginapp/SignUpActivity.java
+++ app/src/main/java/com/firstapp/loginapp/SignUpActivity.java
@@ -1,14 +1,23 @@
 package com.firstapp.loginapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Objects;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Setup custom actionbar
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
     }
 }
