76276847dcf73503daa4d224fe2ce3fba7f83170
harsha <harshavardhanreddyb07@gmail.com>
2022-05-25 18:13:35 +0530

    Signup worked(including backend)

--- app/src/main/java/com/firstapp/loginapp/SignUpActivity.java
+++ app/src/main/java/com/firstapp/loginapp/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.firstapp.loginapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,8 +14,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
 
@@ -96,6 +102,32 @@ public class SignUpActivity extends AppCompatActivity {
 
     private void registerUser(String email, String password) {
         progressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            //If signin successful, then dismiss the progressbar
+                            progressDialog.dismiss();
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Toast.makeText(SignUpActivity.this, "Registered...\n"+user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(SignUpActivity.this, HomePage.class));
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            progressDialog.dismiss();
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
     }
 
 
