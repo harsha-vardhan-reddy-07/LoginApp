fe6e8b996ae4c05a90b4c212d2f3dae0451c89f1
harsha <harshavardhanreddyb07@gmail.com>
2022-05-25 15:55:33 +0530

    login template finished

--- app/src/main/java/com/firstapp/loginapp/SignUpActivity.java
+++ app/src/main/java/com/firstapp/loginapp/SignUpActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Objects;
@@ -17,24 +19,35 @@ public class SignUpActivity extends AppCompatActivity {
     //Setup custom actionbar
     private Toolbar toolbar;
 
+    //Views
+    EditText mNameEt, mEmailEt, mPasswordEt;
+    Button mSignupBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        //init
+
+
 
     }
+
+    //Continue with google button
 
     public void Google_btn(View view) {
         Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
         startActivity(intent1);
     }
 
+    // Signup to Login Page Link
+
     public void Login_Page(View view) {
 
         Intent intent2 = new Intent(this, login_activity.class);
         startActivity(intent2);
-    };
+    }
 
 
 }
