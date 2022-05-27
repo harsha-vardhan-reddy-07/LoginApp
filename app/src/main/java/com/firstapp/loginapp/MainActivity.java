dc99ffd0eba7a8380ea9b0ee6160aff5925d7e7f
harsha <harshavardhanreddyb07@gmail.com>
2022-05-27 23:54:29 +0530

    Splash screen added

--- app/src/main/java/com/firstapp/loginapp/MainActivity.java
+++ app/src/main/java/com/firstapp/loginapp/MainActivity.java
@@ -4,21 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
+import java.util.logging.LogRecord;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static int SPLASH_SCREEN = 4000;
+
+    Handler handler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, HomePage.class));
+                finish();
+            }
+        }, 4000);
 
 
-    // Intent: move to another activity an clicking a button
-    // add Onclick = "Signup_page" to button in xml file
-    public void SignUp_page(View view) {
-        Intent intent = new Intent(this, HomePage.class);
-        startActivity(intent);
+
     }
 }
