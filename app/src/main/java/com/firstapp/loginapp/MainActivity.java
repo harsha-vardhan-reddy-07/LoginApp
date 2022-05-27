8b2894b9485b446ee84d96f088d39dc54f42361c
harsha <harshavardhanreddyb07@gmail.com>
2022-05-28 00:08:31 +0530

    Splash screen statusbar color updated

--- app/src/main/java/com/firstapp/loginapp/MainActivity.java
+++ app/src/main/java/com/firstapp/loginapp/MainActivity.java
@@ -1,11 +1,13 @@
 package com.firstapp.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.Window;
 
 import java.util.logging.LogRecord;
 
@@ -30,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 4000);
 
+        Window window = MainActivity.this.getWindow();
+        window.setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.Splash_screen_status));
 
 
     }
